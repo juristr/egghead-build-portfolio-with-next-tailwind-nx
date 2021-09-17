581ec2c5631b9960a3fcfe482119bc6b7811fa2f
Juri <juri.strumpflohner@gmail.com>
2021-09-17 19:39:01 +0200

    create about page

---
+++ apps/site/pages/about.tsx
@@ -0,0 +1,14 @@
+import './about.module.css';
+
+/* eslint-disable-next-line */
+export interface AboutProps {}
+
+export function About(props: AboutProps) {
+  return (
+    <div>
+      <h1>Welcome to About!</h1>
+    </div>
+  );
+}
+
+export default About;
