01ca21794ab6afe5c7cc6e55670281cb0da0ee7b
Juri <juri.strumpflohner@gmail.com>
2021-09-30 16:21:44 +0200

    tailwind jit mode

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -3,6 +3,7 @@ const { join } = require('path');
 const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 module.exports = {
+  mode: 'jit',
   purge: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
