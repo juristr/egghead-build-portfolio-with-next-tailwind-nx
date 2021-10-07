6f57e0f0b1600ec4c133b0051ffd11838c96c8d0
Juri <juri.strumpflohner@gmail.com>
2021-10-07 11:11:07 +0200

    e2e for nextjs app

--- apps/site-e2e/src/integration/app.spec.ts
+++ apps/site-e2e/src/integration/app.spec.ts
@@ -1,13 +1,11 @@
-import { getGreeting } from '../support/app.po';
-
 describe('site', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/articles/dynamic-routing'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should render the title of the article', () => {
+    cy.get('h1').should('contain', 'Dynamic Routing and Static Generation');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to site!');
+  it('should properly render the embedded Youtube component', () => {
+    cy.get('iframe').should('be.visible');
   });
 });
